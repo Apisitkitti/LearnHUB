3a8f3d6dea74705a95178937015d7db1273279cf
LAPTOP-VBON3RIL\Napat Kemachaiwead <napat.kemachaiwead@mail.kmutt.ac.th>
2023-11-27 11:22:40 +0700

    .

--- server.js
+++ server.js
@@ -59,9 +59,9 @@ const queryDB = (sql) => {
 var checkRegister = false;
 
 app.post('/regisDB', async (req, res) => {
-  let sql = "CREATE TABLE IF NOT EXISTS userInfo (id INT AUTO_INCREMENT PRIMARY KEY, User_Date TIMESTAMP, User_Name VARCHAR(300), User_Email VARCHAR(300), username VARCHAR(300), password VARCHAR(300))";
+  let sql = "CREATE TABLE IF NOT EXISTS userInfo (id INT AUTO_INCREMENT PRIMARY KEY, User_Date TIMESTAMP, User_Name VARCHAR(300), User_Email VARCHAR(300), User_Username VARCHAR(300), password VARCHAR(300))";
   let result = await queryDB(sql);
-  sql = `Select username from userInfo`;
+  sql = `Select User_Username from userInfo`;
   result = await queryDB(sql)
   result = Object.assign({}, result);
   var keys = Object.keys(result);
@@ -69,7 +69,7 @@ app.post('/regisDB', async (req, res) => {
 
   if (req.body.password == req.body.confirm_password && checkRegister == false) {
     let now_date = new Date().toISOString().slice(0, 19).replace('T', ' ');
-    sql = `INSERT INTO userInfo (User_Date, User_Name, User_Email, username, password) VALUES ("${now_date}","${req.body.name}","${req.body.email}","${req.body.username}", "${req.body.password}")`;
+    sql = `INSERT INTO userInfo (User_Date, User_Name, User_Email, User_Username, password) VALUES ("${now_date}","${req.body.name}","${req.body.email}","${req.body.username}", "${req.body.password}")`;
     result = await queryDB(sql);
     check = true;
     checkRegister = true;
@@ -80,7 +80,7 @@ app.post('/regisDB', async (req, res) => {
     for (var user_num = 0; user_num < keys.length; user_num++) {
       if (req.body.password == req.body.confirm_password && req.body.username !== result[keys[user_num]].username) {
         let now_date = new Date().toISOString().slice(0, 19).replace('T', ' ');
-        sql = `INSERT INTO userInfo (User_Date, User_Name, User_Email, username, password) VALUES ("${now_date}","${req.body.name}","${req.body.email}","${req.body.username}", "${req.body.password}")`;
+        sql = `INSERT INTO userInfo (User_Date, User_Name, User_Email, User_Username, password) VALUES ("${now_date}","${req.body.name}","${req.body.email}","${req.body.username}", "${req.body.password}")`;
         result = await queryDB(sql);
         check = true;
         return res.redirect('html/login.html');
@@ -94,21 +94,21 @@ app.post('/regisDB', async (req, res) => {
 })
 
 app.post('/CheckLogin', async (req, res) => {
-  let sql = "CREATE TABLE IF NOT EXISTS userInfo (id INT AUTO_INCREMENT PRIMARY KEY, User_Date TIMESTAMP, User_Name VARCHAR(300), User_Email VARCHAR(300), username VARCHAR(300), password VARCHAR(300))";
+  let sql = "CREATE TABLE IF NOT EXISTS userInfo (id INT AUTO_INCREMENT PRIMARY KEY, User_Date TIMESTAMP, User_Name VARCHAR(300), User_Email VARCHAR(300), User_Username VARCHAR(300), password VARCHAR(300))";
   let result = await queryDB(sql);
-  sql = `Select username, password from userInfo`;
+  sql = `Select User_Username, password from userInfo`;
   result = await queryDB(sql);
   result = Object.assign({}, result);
   var keys = Object.keys(result);
   var check = false;
 
   for (var counter = 0; counter < keys.length; counter++) {
-    if (req.body.username == result[keys[counter]].username &&
+    if (req.body.username == result[keys[counter]].User_Username &&
       req.body.password == result[keys[counter]].password) {
       var check = true;
-      res.cookie("username", result[keys[counter]].username);
+      res.cookie("username", result[keys[counter]].User_Username);
       res.cookie("password", result[keys[counter]].password);
-      console.log(res.cookie("username", result[keys[counter]].username));
+      console.log(res.cookie("username", result[keys[counter]].User_Username));
       return res.redirect('html/index.html');
     }
   }
@@ -119,17 +119,17 @@ app.post('/CheckLogin', async (req, res) => {
 })
 
 app.post('/UpdateDB', async (req, res) => {
-  let sql = "CREATE TABLE IF NOT EXISTS userInfo (id INT AUTO_INCREMENT PRIMARY KEY, User_Date TIMESTAMP, User_Name VARCHAR(300), User_Email VARCHAR(300), username VARCHAR(300), password VARCHAR(300))";
+  let sql = "CREATE TABLE IF NOT EXISTS userInfo (id INT AUTO_INCREMENT PRIMARY KEY, User_Date TIMESTAMP, User_Name VARCHAR(300), User_Email VARCHAR(300), User_Username VARCHAR(300), password VARCHAR(300))";
   let result = await queryDB(sql);
-  sql = `Select username from userInfo`;
+  sql = `Select User_Username from userInfo`;
   result = await queryDB(sql);
   result = Object.assign({}, result);
   var keys = Object.keys(result);
   var check = false;
   for (var information_num = 0; information_num < keys.length; information_num++) {
-    if (req.body.username == result[keys[information_num]].username &&
+    if (req.body.username == result[keys[information_num]].User_Username &&
       req.body.password == req.body.confirm_password) {
-      let sql = `UPDATE userInfo SET password = '${req.body.password}' WHERE username = '${req.body.username}'`;
+      let sql = `UPDATE userInfo SET password = '${req.body.password}' WHERE User_Username = '${req.body.username}'`;
       let result = await queryDB(sql);
       check = true;
       return res.redirect('html/login.html');
